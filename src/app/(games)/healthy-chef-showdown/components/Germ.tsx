a6774b1864b6820abe8015ea7be555603088a7e6
vickvey <vickvey0303@gmail.com>
2025-10-26 18:08:35 +0530

    no linting error

--- src/app/(games)/healthy-chef-showdown/components/Germ.tsx
+++ src/app/(games)/healthy-chef-showdown/components/Germ.tsx
@@ -1,7 +1,6 @@
-
-import React from 'react';
-import { Germ, GermVariant } from '../types';
-import { GERM_VARIANTS } from '../constants';
+import React from "react";
+import { Germ } from "../types";
+import { GERM_VARIANTS } from "../constants";
 
 interface GermProps {
   germ: Germ;
@@ -16,19 +15,26 @@ const GermComponent: React.FC<GermProps> = ({ germ, onBust }) => {
     setIsBusted(true);
     // The animation takes 300ms, so we wait before removing it from the DOM
     setTimeout(() => {
-        onBust(germ.id, variantDetails.points);
+      onBust(germ.id, variantDetails.points);
     }, 300);
   };
 
-  const animationClass = germ.variant === 'pink' ? 'animate-wiggle animation-duration-500' : 'animate-wiggle';
+  const animationClass =
+    germ.variant === "pink"
+      ? "animate-wiggle animation-duration-500"
+      : "animate-wiggle";
 
   return (
     <div
-      className={`absolute cursor-pointer transition-transform duration-200 hover:scale-110 ${isBusted ? 'animate-pop' : ''}`}
+      className={`absolute cursor-pointer transition-transform duration-200 hover:scale-110 ${
+        isBusted ? "animate-pop" : ""
+      }`}
       style={{ top: `${germ.y}%`, left: `${germ.x}%` }}
       onClick={handleClick}
     >
-      <variantDetails.Icon className={`${variantDetails.size} ${animationClass}`} />
+      <variantDetails.Icon
+        className={`${variantDetails.size} ${animationClass}`}
+      />
     </div>
   );
 };
--- src/app/api/coins/route.ts
+++
@@ -1,5 +0,0 @@
-export function GET() {
-  return Response.json({
-    game_coins: 1001.24,
-  });
-}
